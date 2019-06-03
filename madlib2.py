5f3a33c35449ea97714db42e36ac5a93cb8ccbf2
larry <lmoiola@gmail.com>
2019-06-03 12:25:41 -0700

    update all files

---
+++ madlib2.py
@@ -0,0 +1,10 @@
+# "That rug really tied the room together."
+'''
+This is a comment
+'''
+# this is one-line comment
+
+madlib_decor = input("Enter a piece of furniture: ")
+madlib_verb = input("Enter a verb: ")
+madlib_room = input("Enter a type of room: ")
+print (f"That {madlib_decor} really {madlib_verb} the {madlib_room} together.")
---
+++ turtle_lab1.py
@@ -0,0 +1,31 @@
+# turtle_lab1.py
+
+'''import turtle
+turtle.forward(100)
+turtle.done()
+
+# too cumbersom
+from turtle import *
+forward(100)
+done()
+'''
+
+import turtle
+# head
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+# move to neck
+turtle.penup()
+turtle.forward(25)
+turtle.pendown()
+# neck
+turtle.right(90)
+
+# the end
+turtle.done()

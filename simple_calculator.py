7d18e5c5d489a5514bea485734b2d4eb14861929
larry <lmoiola@gmail.com>
2019-06-06 14:56:29 -0700

    lab 11, simple calc with input validation

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,10 +1,20 @@
+def get_float(text):
+    while True:
+        v = input(text)
+        try:
+            v = float(v)
+            return v
+        except ValueError:
+            print('That value in invalid!')
+
+
 while True:
     user_operator = input("What is the operation you'd like to perform?\n")
     if user_operator.upper() == 'DONE':
         break
-        
-    user_input1 = float(input("What is the first number?\n"))
-    user_input2 = float(input("What is the second number?\n"))
+
+    user_input1 = get_float("What is the first number?\n")
+    user_input2 = get_float("What is the second number?\n")
 
 
     if user_operator in '+-*/':
@@ -17,3 +27,5 @@ while True:
             print("The result is: " +str(user_input1 * user_input2))
         elif user_operator == '/':
             print("The result is: " +str(user_input1 / user_input2))
+    else:
+        print('That is not a valid operator')

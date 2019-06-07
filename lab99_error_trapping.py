fdc16817b041e5bed11332daf37e49210da847eb
larry <lmoiola@gmail.com>
2019-06-06 21:32:55 -0700

    fix for Changes not staged for commit:

--- lab99_error_trapping.py
+++ lab99_error_trapping.py
@@ -1,8 +1,14 @@
 my_dict = {'key': 'value'}
 while True:
-    user_input = input ('Type key: ')
+    user_input = input ("Type 'key': ")
     try:
         print(my_dict[user_input])
         break
     except KeyError:
         print('wrong')
+
+while True:
+    user_num = input('Type a number: ')
+    if user_num.isdigit():
+        print(int(user_num) + 1)
+        break

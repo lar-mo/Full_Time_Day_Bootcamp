032f3bf1d15ad3937dabf9f420b9526a30b5b6c7
Larry Moiola <lmoiola@gmail.com>
2019-06-14 14:33:34 -0700

    Delete lab99_hint_madlib.py

--- lab99_hint_madlib.py
+++
@@ -1,6 +0,0 @@
-# "Hello, my name is Inigo Montoya. You killed my father. Prepare to die."
-
-madlib_name = input("Give me a name: ")
-madlib_relative = input("Give me a type of relative: ")
-madlib_verb = input("Give me a verb (nontransitive): ")
-print(f"Hello, my name is {madlib_name}. You killed my {madlib_relative}. Prepare to {madlib_verb}.")

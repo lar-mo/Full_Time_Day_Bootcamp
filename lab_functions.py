ee5662696bd31fa13206f75ae3b02ddf1a96ba2e
larry <lmoiola@gmail.com>
2019-06-23 20:05:44 -0700

    moving individual run_test() functions to a single lab_functions.py

--- lab_functions.py
+++ lab_functions.py
@@ -1,9 +1,9 @@
-# filename: lab17_functions.py
+# filename: lab_functions.py
 '''
-This file contains two functions: run_tests() & run_tests_p().
-    • run_tests_1() is for the Anagram & Credit Card Validation units tests.
+This file contains two Run Tests functions: run_tests_1() & run_tests_2().
+    • run_tests_1() is for the Anagram, Blackjack Advice, & Credit Card Validation units tests.
     • run_tests_2() is for the Palindrome units.
-    • TODO: add the input section from v1 and v1 of the labs, if time allows.
+    • TODO: add the input section(s) from the labs, if time allows.
 '''
 
 '''
@@ -12,37 +12,37 @@ This file contains two functions: run_tests() & run_tests_p().
 #######################################
 
 This function takes two parameters, user input & expected output (list of tuples) and function_name,
-submits each test_input(string) to the function-name under-test, then
-returns the result of comparison: actual output vs. expected output.
+submits each test_input(string, strings, or tuple) to the function-name under-test, then
+returns the result of comparison: actual output (Boolean or string) vs. expected output (Boolean or string).
 
 e.g. Anagram (lab 17)
-input_output = [(string, expected_output] # 1-parameter input
-[(1, 'racecar', True),    # base case (no capital letters, spaces, or non-ascii letters)
- (2, 'Race car', True),   # capital letters are converted to lowercase
- (3, 'racecar!', True),   # non-ascii letters are ignored
- (4, 'racecar', False)]   # the second 'a' and 'o' are transposed
+# input_output = [(string, expected_output] # 1-parameter input
+# (1, 'racecar', True),    # base case (no capital letters, spaces, or non-ascii letters)
+# (2, 'Race car', True),   # capital letters are converted to lowercase
+# (3, 'racecar!', True),   # non-ascii letters are ignored
+# (4, 'racecar', False)]   # the second 'a' and 'o' are transposed
 
 e.g. Blackjack Advice (lab 19)
-input_output => [(test number, [list of poker cards], expected result string)] # 1-parameter input
+# input_output => [(test number, [list of poker cards], expected result string)] # 1-parameter input
 ### v1 - Ace is always worth 1 ###
-# (1, ['A', '8', '2'], '11 Hit'),            # 11
-# (2, ['K', '3', '5'], '18 Stay'),           # 18
-# (3, ['J', 'Q', 'A'], '21 Blackjack!'),     # 21
-# (4, ['9', '7', '6'], '22 Already busted!') # 22
+# (1, ['A', '8', '2'], '11 Hit'),            # Hit
+# (2, ['K', '3', '5'], '18 Stay'),           # Stay
+# (3, ['J', 'Q', 'A'], '21 Blackjack!'),     # Blackjack!
+# (4, ['9', '7', '6'], '22 Already busted!') # Already busted!
 
 ### v2 - Ace can be worth 1 or 11 ###
-# (1, ['2', '4', '8'], '14 Hit'),            # 14
-# (2, ['A', 'A', 'A'], '13 Hit'),            # 13, three As (11+1+1) - lines 40-44
-# (3, ['A', 'A', '4'], '16 Hit'),            # 16, two As (11+1) - lines 35-39
-# (4, ['K', '3', '5'], '18 Stay'),           # 18
-# (5, ['J', 'Q', 'A'], '21 Blackjack!'),     # 21, one A (1) - lines 30-34
-# (6, ['A', '8', '2'], '21 Blackjack!'),     # 21, one A (11) - lines 30-34
-# (7, ['9', '7', '6'], '22 Already busted!') # 22
+# (1, ['2', '4', '8'], '14 Hit'),            # Hit
+# (2, ['A', 'A', 'A'], '13 Hit'),            # Hit, three As (11+1+1) - lines 40-44
+# (3, ['A', 'A', '4'], '16 Hit'),            # Hit, two As (11+1) - lines 35-39
+# (4, ['K', '3', '5'], '18 Stay'),           # Stay
+# (5, ['J', 'Q', 'A'], '21 Blackjack!'),     # Blackjack!, one A (1) - lines 30-34
+# (6, ['A', '8', '2'], '21 Blackjack!'),     # Blackjack!, one A (11) - lines 30-34
+# (7, ['9', '7', '6'], '22 Already busted!') # Already busted!
 
 e.g. Credit Card Valdation
-input_output => [(test number, [list of credit card numbers], expected result string)] # 1-parameter input
-# [(1, "5392799316110230", True),  # MasterCard
-#  (2, "4971023049709545", True)]  # Visa
+# input_output => [(test number, [list of credit card numbers], expected result string)] # 1-parameter input
+# (1, "5392799316110230", True),  # MasterCard
+# (2, "4971023049709545", True)   # Visa
 
 If any tests fail, a descriptive message is returned and printed.
 Otherwise, "All tests passed." is returned and printed.
@@ -74,11 +74,11 @@ submits first_string and second_string to the function-name under-test, then
 returns the result of comparison: actual output vs. expected output.
 
 e.g. Palindrome (lab 17)
-input_output = [(test_number, first string, second string, expected_output)] # 2-parameter input
-[(1, 'nude dragons', 'soundgarden', True),         # input includes spaces
- (2, 'Brag', 'Grab!', True),                       # input includes capital letters and punctuation
- (3, 'The Morse Code', 'Here comes dots', False),  # length mismatch
- (4, 'Angel', 'Gleam', False)]                     # letters don't match
+# input_output = [(test_number, first string, second string, expected_output)] # 2-parameter input
+# (1, 'nude dragons', 'soundgarden', True),         # input includes spaces
+# (2, 'Brag', 'Grab!', True),                       # input includes capital letters and punctuation
+# (3, 'The Morse Code', 'Here comes dots', False),  # length mismatch
+# (4, 'Angel', 'Gleam', False)]                     # letters don't match
 
 If any tests fail, a descriptive message is returned and printed.
 Otherwise, "All tests passed." is returned and printed.
@@ -88,14 +88,14 @@ def run_tests_2(input_output, function_name):
     failed_test_count = 0
     failed_test_msg = ''
     for i in range(len(input_output)):
-        test_number =
+        test_number = input_output[i][0]
         first_string = input_output[i][1]
         second_string = input_output[i][2]
         function_output = function_name(first_string, second_string)
-        expected_output = input_output[i][0]
+        expected_output = input_output[i][3]
         if function_output != expected_output:
             failed_test_count += 1
-            failed_test_msg += f"\nInput: {first_string, second_string}\nFail. Expected Result: {expected_output} ==>   Actual result: {function_output}\n"
+            failed_test_msg += f"\nTest #{test_number} - Failed\nInput: {first_string, second_string}\n==> Expected Result: {expected_output}\n==>   Actual result: {function_output}\n"
     if failed_test_count != 0:
         return failed_test_msg
     if failed_test_count == 0:

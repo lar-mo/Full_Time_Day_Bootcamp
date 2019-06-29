b6045f46000096219a13aa0e63c635b6b107b628
larry <lmoiola@gmail.com>
2019-06-28 17:53:56 -0700

    removing files not in class_emu

--- lab09_hint_written_addition.py
+++
@@ -1,7 +0,0 @@
-# filename: written_addition.py
-# Hint for Lab 9: Unit Converter
-
-num_dict = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5}
-first_num = input("What is the first number? ")
-second_num = input("What is the second number? ")
-print(f"The sum is {num_dict[first_num] + num_dict[second_num]}")

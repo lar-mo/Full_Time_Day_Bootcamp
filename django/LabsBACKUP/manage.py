751ab825622c3d6570d830dd3b7b071ad709ee8b
larry <lmoiola@gmail.com>
2019-07-28 12:00:29 -0700

    savepoint for contactsApp

--- django/LabsBACKUP/manage.py
+++ django/LabsBACKUP/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todoproj.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myProjects.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- django/todoproj/todoapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class TodoItem(models.Model):
-    text = models.CharField(max_length=200)
-    # models.TextField() # unlimited length** within reason
-    completed = models.BooleanField()
-
-    def __str__(self):
-        return self.text

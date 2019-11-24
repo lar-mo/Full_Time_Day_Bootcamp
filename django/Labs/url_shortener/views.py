744da07e2531e2bf25c404f8a6560ece2098faa6
larry <lmoiola@gmail.com>
2019-11-23 19:50:51 -0800

    simplify eval logic:  >=1 is now >0 (line 25)

--- django/Labs/url_shortener/views.py
+++ django/Labs/url_shortener/views.py
@@ -22,7 +22,7 @@ def saveurl(request):
         for i in range(5):
             shorturl_code += random.choice(letters_digits)
         exist_count = shortened_url.objects.filter(code=shorturl_code).count()
-        if exist_count >= 1:
+        if exist_count > 0:
             print("shorturl_code already exists")
         else:
             short_url = shortened_url(code=shorturl_code, long_url=long_url)
